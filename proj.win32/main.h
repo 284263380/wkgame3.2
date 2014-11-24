8713d2d35c41e8d6515b8f09511a2a383d8ec622
syxcools <syxcools@163.com>
2014-11-24 12:23:20 +0800

    add

---
+++ proj.win32/main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files:
+#include <windows.h>
+#include <tchar.h>
+
+// C RunTime Header Files
+#include "CCStdC.h"
+#pragma warning (disable : 4204)
+#pragma warning (disable : 4819)
+#pragma warning (disable : 4018)
+#pragma warning (disable : 4996)
+
+
+#endif    // __MAIN_H__
